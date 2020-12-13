ffbf589aa9c96c04dacd6a1682c93ab5285ba76b
Gustavo <gustavoemanuel901@gmail.com>
2020-12-13 11:19:41 -0300

    Add Styles

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,28 @@ export default {
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
 
+  body {
+    color: #ffffff;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21,87,153))
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 68px;
+  }
 </style>
